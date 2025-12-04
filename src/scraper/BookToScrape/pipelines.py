b906eaa1842ef12cbec97872ae426d7d2d158d17
vcleyton <cvmarquesbarroca26@gmail.com>
2025-12-04 20:55:08 -0300

    Basic structure implemented

---
+++ src/scraper/BookToScrape/pipelines.py
@@ -0,0 +1,54 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import sqlite3
+import os
+
+class BooktoscrapePipeline:
+    def open_spider(self, spider):
+        # DB file created in current working dir (where you run `scrapy crawl`)
+        db_path = os.path.join(os.getcwd(), "books.db")
+        self.conn = sqlite3.connect(db_path)
+        self.cur = self.conn.cursor()
+        self.cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS books (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT,
+                price TEXT,
+                stock INTEGER,
+                rating TEXT,
+                category TEXT,
+                image_url TEXT
+            )
+            """
+        )
+        self.conn.commit()
+
+    def close_spider(self, spider):
+        self.conn.commit()
+        self.conn.close()
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        self.cur.execute(
+            """
+            INSERT INTO books (title, price, stock, rating, category, image_url)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            (
+                adapter.get("title"),
+                adapter.get("price"),
+                adapter.get("stock"),
+                adapter.get("rating"),
+                adapter.get("category"),
+                adapter.get("image_url"),
+            ),
+        )
+        self.conn.commit()
+        return item
--- src/scraper/BookToScrape/settings.py
+++ src/scraper/BookToScrape/settings.py
@@ -58,9 +58,9 @@ DOWNLOAD_DELAY = 1
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "BookToScrape.pipelines.BooktoscrapePipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "BookToScrape.pipelines.BooktoscrapePipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
