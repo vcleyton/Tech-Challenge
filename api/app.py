96541c03c1ce452b8f257e9f36f2cdd84395a88e
Jonatas Locateli <jonataslocateli@gmail.com>
2026-01-10 22:27:02 -0300

    Projeto Books Tech Challenge pronto para produção

--- api/app.py
+++ api/app.py
@@ -77,8 +77,8 @@ def create_app():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
     app = create_app()
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
     #app = create_app()
     #app.run(debug=True)
