8c8d92ac6de0f5cddb5ba37a28014379e44753e4
Jonatas Locateli <jonataslocateli@gmail.com>
2026-01-17 15:06:10 -0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,15 @@
 import pytest
-from api.app import create_app  # IMPORTAR A FUNÇÃO, NÃO O OBJETO app
+from api.app import create_app 
 
 @pytest.fixture
 def app():
     """
-    Fixture que cria uma instância da aplicação Flask para testes
+    Cria uma instância da aplicação Flask para testes
     """
-    app = create_app()  # Usa create_app() para ter todos os Namespaces e configs
+    app = create_app() 
     app.config.update({
-        "TESTING": True,      # ativa modo de teste
-        "JWT_SECRET_KEY": "super-secret-key"  # garante compatibilidade JWT
+        "TESTING": True,      
+        "JWT_SECRET_KEY": "super-secret-key" 
     })
 
     with app.app_context():
@@ -18,6 +18,6 @@ def app():
 @pytest.fixture
 def client(app):
     """
-    Fixture que cria um client de testes para fazer requests à API
+    Cria um client de testes para fazer requests à API
     """
     return app.test_client()
